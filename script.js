50c51d1d82a68e4e765f4e8df973fc3ef99ef62f
Emmanuel Mwamuka <a.emmanuelll.17@gmail.com>
2025-06-22 21:53:14 +0200

    Added status input and lettercase transformation

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ async function getTask(taskNumber) {
 
   const description = prompt(`Enter the description for "${title}":`);
   if (description === null) return null;
+
+  let status = prompt(`Enter the status for "${title}" (todo, doing, done):`);
+  if (status === null) return null;
+
+  status = status.toLowerCase();
 }

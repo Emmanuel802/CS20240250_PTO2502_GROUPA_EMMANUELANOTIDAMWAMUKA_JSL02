f6398df32926e27dc63c7034ec7e9ed3f18e39c2
Emmanuel Mwamuka <a.emmanuelll.17@gmail.com>
2025-06-22 21:51:14 +0200

    Added description input

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 async function getTask(taskNumber) {
   const title = prompt(`Enter task ${taskNumber} title:`);
   if (title === null) return null;
+
+  const description = prompt(`Enter the description for "${title}":`);
+  if (description === null) return null;
 }
